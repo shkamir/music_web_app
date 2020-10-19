fae98f5e92103718f4cb215b06c8ac8ce607edc6
Amirhossein Shekooh <shekooha696@gmail.com>
2020-10-19 11:53:34 +0330

    made the title dynamic

--- music/ahang/views.py
+++ music/ahang/views.py
@@ -6,9 +6,11 @@ def index(request):
     ahangha = Ahang.objects.all().order_by('-updateDate')
     context = {
         "ahangha": ahangha,
+        "title": "Home",
     }
     return render(request, 'main/index.html', context)
 
 def ahang_detail(request, id=None):
     ahang = get_object_or_404(Ahang, id=id)
-    return render(request,'main/ahang_detail.html', {"ahang":ahang})
+    esme_ahang = f"{ahang.author}\t------\t{ahang.ahang_esm}"
+    return render(request,'main/ahang_detail.html', {"ahang":ahang, "title":esme_ahang})
