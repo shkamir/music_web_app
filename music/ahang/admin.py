3a7b39f1aa02b3ea0ea88d0bc0446e6b21dbc180
Amirhossein Shekooh <shekooha696@gmail.com>
2020-11-06 11:05:02 +0330

    added 2 actions for Ahang admin panel

--- music/ahang/admin.py
+++ music/ahang/admin.py
@@ -3,15 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import Ahang, CommentDb
 
-
+@admin.register(Ahang)
 class AhangManager(admin.ModelAdmin):
     list_display = ('author','ahang_esm',"isAgreed",)
     list_filter = ('publishDate',"isAgreed",)
     list_editable = ("isAgreed",)
     search_fields = ('author','ahang_esm',)
+    actions=["active_ahang", "deactivate_ahang",]
     class Meta:
         model = Ahang
+    def active_ahang(self, request, queryset):
+        queryset.update(isAgreed=True)
 
+    def deactivate_ahang(self, request, queryset):
+        queryset.update(isAgreed=False)
 
 @admin.register(CommentDb)
 class CommentAdmin(admin.ModelAdmin):
@@ -23,4 +28,3 @@ class CommentAdmin(admin.ModelAdmin):
     def approve_comments(self, request, queryset):
         queryset.update(active=True)
 
-admin.site.register(Ahang, AhangManager)
